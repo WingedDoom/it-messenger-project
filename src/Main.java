dc3cbbb9e492aa1264ae5d085cd47b9e786c2b0b
timkabor <borgalinov@gmail.com>
2017-11-20 17:10:53 +0300

    LZ77 half ready

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import Coding.CyclicRedundancyCheck;
+import Compression.LZ77;
 
 import javax.print.DocFlavor;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
         Class main is used for starting a messenger session.
         User is expected to run the messenger with 1 argument: his session id.
@@ -14,7 +16,6 @@ public class Main {
         After that, socket session is created and main constantly waits for console input and prints
         any messages received from the outside.
          */
-
     }
 
 }
